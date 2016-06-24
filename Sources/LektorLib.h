0fbf770ac0da07bbd0d8df60bf50e5ddac0c8d1f
Vladimir Urbano <l@pantanourbano.com>
2016-06-24 12:04:07 -0400

    Removed target folders

---
+++ Sources/LektorLib.h
@@ -0,0 +1,19 @@
+//
+//  LektorLib.h
+//  LektorLib
+//
+//  Created by Vladimir Urbano on 6/24/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LektorLib.
+FOUNDATION_EXPORT double LektorLib_VersionNumber;
+
+//! Project version string for LektorLib.
+FOUNDATION_EXPORT const unsigned char LektorLib_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LektorLib/PublicHeader.h>
+
+
