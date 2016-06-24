8547065959ae5eb94ee1f0684fc25826cba692b2
Vladimir Urbano <l@pantanourbano.com>
2016-06-24 14:23:14 -0400

    Adding unit tests

---
+++ LektorLib iOSExample/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  LektorLib iOSExample
+//
+//  Created by Vladimir Urbano on 6/24/16.
+//
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
