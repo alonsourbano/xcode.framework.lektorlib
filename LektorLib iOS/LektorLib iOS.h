81364bdb9021e519fcf51ece97db25f7c831ac22
Vladimir Urbano <l@pantanourbano.com>
2016-06-24 11:24:35 -0400

    Added targets

---
+++ LektorLib iOS/LektorLib iOS.h
@@ -0,0 +1,19 @@
+//
+//  LektorLib iOS.h
+//  LektorLib iOS
+//
+//  Created by Vladimir Urbano on 6/24/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LektorLib iOS.
+FOUNDATION_EXPORT double LektorLib_iOSVersionNumber;
+
+//! Project version string for LektorLib iOS.
+FOUNDATION_EXPORT const unsigned char LektorLib_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LektorLib_iOS/PublicHeader.h>
+
+
---
+++ LektorLib macOS/LektorLib macOS.h
@@ -0,0 +1,19 @@
+//
+//  LektorLib macOS.h
+//  LektorLib macOS
+//
+//  Created by Vladimir Urbano on 6/24/16.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LektorLib macOS.
+FOUNDATION_EXPORT double LektorLib_macOSVersionNumber;
+
+//! Project version string for LektorLib macOS.
+FOUNDATION_EXPORT const unsigned char LektorLib_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LektorLib_macOS/PublicHeader.h>
+
+
